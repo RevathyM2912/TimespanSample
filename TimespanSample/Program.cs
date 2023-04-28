ef25272e896ca39ac5c37fa1e2e66b69d57cbf60
AzureAD\RevathyM <revathy.m@williamslea.com>
2023-04-28 19:23:07 +0530

    First commit : Timespan sample check properties

--- TimespanSample/Program.cs
+++ TimespanSample/Program.cs
@@ -2,6 +2,26 @@
 {
     private static void Main(string[] args)
     {
-        
+        try
+        {
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(3);
+
+            var duration = end - start;
+            Console.WriteLine($"Duration : {duration}");
+
+            //Properties of TimeSpan
+            var timeSpan = new TimeSpan(1, 30, 2);
+            Console.WriteLine($"Minutes : {timeSpan.Minutes}");
+            Console.WriteLine($"Total minutes : {timeSpan.TotalMinutes}");
+            Console.WriteLine($"Total hours : {timeSpan.TotalHours}");
+
+            //Add
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
